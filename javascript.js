cd6cd0f3a48c86f9c166c650072a2f81557fe1fb
Nishan <arifuzzamannishan@gmail.com>
2021-01-30 20:42:46 +0600

    ticket information part done

--- javascript.js
+++ javascript.js
@@ -1,16 +1,36 @@
+//global variable declare
 var subTotal = 0;
 var amountForSClass = 0;
 var amountForEconomyClass = 0;
+var ticketQuantity;
+var vatCal = 0;
+var totalAmount = 0;
+const fClassPrice = 150;
+const sClassPrice = 100;
 
+
+//it returns the id
 function getId(id) {
     return document.getElementById(id);
 }
 
+
+// it's set the value into the innerText
+function setValueWithOneId(id,value){
+    getId(id).innerText = value;
+}
+
+
+//it returns the ticketQuantity
+//if the positiveNegative value is positive then the ticket quantity increment 
+//if the positiveNegative value is negative then the ticket quantity decrement 
 function ticketQuantityCount(positiveNegativeValue,ticketQuantity) {
     positiveNegativeValue == "positive" ? ticketQuantity++ : ticketQuantity--;
     return ticketQuantity;
 }
 
+
+//it returns the amount
 function getAmount(ticketPrice, ticketQuantity) {
 
     return ticketPrice * ticketQuantity;
@@ -18,7 +38,9 @@ function getAmount(ticketPrice, ticketQuantity) {
 }
 
 
+//it returns the first and second class total price
 function getSubtotal(inputId, ticketPrice, ticketQuantity) {
+
     if (inputId == "f-class-input") {
         amountForSClass = getAmount(ticketPrice, ticketQuantity);
     }
@@ -30,95 +52,121 @@ function getSubtotal(inputId, ticketPrice, ticketQuantity) {
 }
 
 
+// it returns the vat here 0.1 is 10% vat and subtotal is first and second class price sum
 function vatCalculation(subTotal) {
     return 0.1 * subTotal;
 }
 
-var ticketQuantity;
-var vatCal = 0;
-var totalAmount = 0;
 
+//this is the main function it's calculate the main things in our project
+//-->here btnId is which button we want to add eventHandler
+//-->positiveNegativeValue is use to for increment or decrement quantity
+//-->inputId is use to set/get the ticketQuantity value inside the input field
+//-->ticketPrice is first and second class price.
 function eventHandler(btnId, positiveNegativeValue, inputId, ticketPrice) {
 
     getId(btnId).addEventListener("click", function () {
+        
+        //it's takes the value from input field
         ticketQuantity = parseInt(getId(inputId).value);
 
+        //this function is increment or decrement ticket quantity value
         ticketQuantity = ticketQuantityCount(positiveNegativeValue,ticketQuantity);
 
+        //here check my ticket quantity is negative or not if negative then my quantity value is zero and show an alert message.
         if (ticketQuantity < 0) {
             ticketQuantity = 0;
             alert("ticketQuantity cannot be negative");
         }
 
+        //it set the value into input field
         getId(inputId).value = ticketQuantity;
 
+        //this function calculate the sum of two classes prices
         subTotal = getSubtotal(inputId, ticketPrice, ticketQuantity);
-        getId("sub-total").innerText = subTotal;
 
+        //here sub-total is id name and it set the value into subtotal field
+        setValueWithOneId("sub-total",subTotal);
+
+        //it's calculate the vat 
          vatCal = parseFloat(vatCalculation(subTotal).toPrecision(4));
 
-        getId("vat-calculation").innerText = vatCal;
+        // it set the vat and vat-calculation is a id name
+        setValueWithOneId("vat-calculation",vatCal);
 
+        //it set the total amount
         totalAmount = subTotal + vatCal;
-        getId("total-amount").innerText = totalAmount;
+        setValueWithOneId("total-amount",totalAmount);
     })
 
 }
 
 
-eventHandler("f-class-plus-btn", "positive", "f-class-input", 150);
+//this is function call for first class ticket for increment and decrement portion
+eventHandler("f-class-plus-btn", "positive", "f-class-input", fClassPrice);
+eventHandler("f-class-minus-btn", "negative", "f-class-input", fClassPrice);
 
-eventHandler("f-class-minus-btn", "negative", "f-class-input", 150);
+//this is function call for second class ticket for increment and decrement portion
+eventHandler("s-class-plus-btn", "positive", "s-class-input", sClassPrice);
+eventHandler("s-class-minus-btn", "negative", "s-class-input", sClassPrice);
 
-eventHandler("s-class-plus-btn", "positive", "s-class-input", 100);
 
-eventHandler("s-class-minus-btn", "negative", "s-class-input", 100);
-
+//this function is set the value into the innerText field
+// and the getId is a function
+function setValueWithTwoId(id1,id2){
+    getId(id1).innerText = getId(id2).value;
+}
 
 
 function information(){
-    //flying from and flying to
-    getId("info-flying-from").innerText = getId("input-flying-from").value;
-    getId("info-flying-to").innerText = getId("input-flying-to").value;
+    //set value into flying-from and flying-to
+    setValueWithTwoId("info-flying-from","input-flying-from");
+    setValueWithTwoId("info-flying-to","input-flying-to");
     
-    // departure and return
-    getId("info-departure").innerText = getId("input-departure").value;
-    getId("info-return").innerText = getId("input-return").value;
+    //set value into departure and return
+    setValueWithTwoId("info-departure","input-departure");
+    setValueWithTwoId("info-return","input-return");
 
     //first class and second class ticket count
     const fClassTicketQuantity = parseInt(getId("f-class-input").value);
     const sClassTicketQuantity = parseInt(getId("s-class-input").value);
 
-    getId("f-class-ticket-quantity").innerText = fClassTicketQuantity;
-    getId("s-class-ticket-quantity").innerText = sClassTicketQuantity;
+    //set value into ticket quantity
+    setValueWithOneId("f-class-ticket-quantity",fClassTicketQuantity);
+    setValueWithOneId("s-class-ticket-quantity",sClassTicketQuantity);
 
-    //first class and second class ticket price
-    getId("f-class-price").innerText = fClassTicketQuantity*150;
-    getId("s-class-price").innerText = sClassTicketQuantity*100;
+    //set value into first class and second class ticket price
+    // first class ticket price is 150
+    //and second class ticket price is 100
+    setValueWithOneId("f-class-price",fClassTicketQuantity*fClassPrice);
+    setValueWithOneId("s-class-price",sClassTicketQuantity*sClassPrice);
 
-    //total ticket price
-    getId("total-ticket-price").innerText = subTotal;
+    //set value into total ticket price
+    setValueWithOneId("total-ticket-price",subTotal);
 
-    //vat
-    getId("vat").innerText = vatCal;
+    //set value into vat
+    setValueWithOneId("vat",vatCal);
 
-    //total price
-    getId("total-price").innerText = totalAmount;
-
+    //set value into total price
+    setValueWithOneId("total-price",totalAmount);
 }
 
 
+//this function is to show to the all ticket information if someone click in bookNow button and here book-now is a Id of bookNow button.
 const bookNow = getId("book-now");
+
 bookNow.addEventListener("click",function(){
-    var text;
+
     if(confirm("Are you sure you want to book this ticket?")){
-        const bookingSection = getId("first");
+
+        //here first-div-id is the first div of my project and using this id to hide the my first page 
+        const bookingSection = getId("first-div-id");
+
         bookingSection.style.display = "none";
         const info = getId("ticket-info");
         info.style.display = "block";
-        information();
 
-        
+        //this function show all the information after clicking book now button
+        information();  
     }
-    else console.log("no pressed");
 })
